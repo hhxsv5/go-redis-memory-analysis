a464403fe972c7d6b5fc1a0319103277dd6af838
XieBiao <xiebiao@medlinker.com>
2017-11-16 18:32:01 +0800

    example

--- src/examples/main.go
+++ src/examples/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	. "github.com/hhxsv5/go-redis-memory-analysis/src/models"
+	. "github.com/hhxsv5/go-redis-memory-analysis/src"
+)
 
+func main() {
+	redis, err := NewRedisClient("127.0.0.1", 6379, "")
+	if err != nil {
+		fmt.Println("Connect redis fail", err)
+		return
+	}
+	analysis := NewAnalysis(redis)
+	analysis.Start([]string{"#", ":"}, 3000)
+
+	fmt.Println(analysis.Reports)
 }
