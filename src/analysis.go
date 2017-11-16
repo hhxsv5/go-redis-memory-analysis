4ee69081be451ff24a96550f2a2d209717391ffc
XieBiao <xiebiao@medlinker.com>
2017-11-16 17:54:38 +0800

    scr

---
+++ src/analysis.go
@@ -0,0 +1,76 @@
+package gorma
+
+import (
+	"strings"
+	"fmt"
+	"strconv"
+)
+
+type Report struct {
+	Key         string
+	Count       uint64
+	Size        uint64
+	NeverExpire uint64
+	AvgTtl      uint64
+}
+
+type Analysis struct {
+	redis   RedisClient
+	Reports map[int]map[string]Report
+}
+
+func NewAnalysis(redis *RedisClient) (*Analysis) {
+	return &Analysis{redis, map[int]map[string]Report{}}
+}
+
+func (analysis *Analysis) start(delimiters []string, limit uint64) {
+	match := "*[" + strings.Join(delimiters, "") + "]*"
+	cursor := 0
+	keys := analysis.redis.Scan(&cursor, match, 3000)
+
+	fd, fp, tmp, nk := "", 0, 0, ""
+	var r Report
+	var f float64
+	var ttl int64
+	var length uint64
+	for _, key := range keys {
+		fd, fp, tmp, nk, ttl, ne = "", 0, 0, "", 0, 0
+		for _, delimiter := range delimiters {
+			tmp = strings.Index(key, delimiter)
+			if tmp < fp {
+				fd, fp = delimiter, tmp
+			}
+		}
+
+		if _, ok := analysis.Reports[db]; !ok {
+			analysis.Reports[db] = map[string]Report{}
+		}
+
+		nk = key[fp:] + fd + "*"
+
+		if _, ok := analysis.Reports[db][nk]; ok {
+			r = analysis.Reports[db][nk]
+		} else {
+			r = Report{nk, 1, 0, 0, 0}
+		}
+
+		ttl, _ = analysis.redis.Ttl(nk)
+
+		switch ttl {
+		case -2:
+			continue
+		case -1:
+			r.NeverExpire++
+		default:
+			f = float64(r.AvgTtl*(r.Count-r.NeverExpire)+uint64(ttl)) / float64(r.Count+1)
+			ttl, _ := strconv.ParseUint(fmt.Sprintf("%0.0f", f), 10, 64)
+			r.AvgTtl = ttl
+			r.Count++
+		}
+
+		length, _ = analysis.redis.SerializedLength(nk)
+		r.Size += length
+
+		analysis.Reports[db][nk] = r
+	}
+}
---
+++ src/models/redis.go
@@ -0,0 +1,84 @@
+package models
+
+import (
+	"github.com/garyburd/redigo/redis"
+	"fmt"
+	"bytes"
+	"strings"
+	"strconv"
+)
+
+type RedisClient struct {
+	conn
+}
+
+func NewRedisClient(host string, port uint16, password string) (*RedisClient, error) {
+	var addr bytes.Buffer
+	addr.WriteString(host)
+	addr.WriteString(":")
+	addr.WriteString(string(port))
+
+	conn, err := redis.Dial("tcp", addr.String())
+	if err != nil {
+		fmt.Println("connect redis fail: ", err)
+		return nil, err
+	}
+
+	if password != "" {
+		_, err := conn.Do("AUTH", password)
+		if err != nil {
+			fmt.Println("auth fail:", err)
+			return nil, err
+		}
+	}
+
+	return &RedisClient{conn}, err
+}
+
+func (client RedisClient) GetDatabases() ([]string, error) {
+	reply, err := client.conn.Do("INFO", "Keyspace")
+	dbs, err := redis.Strings(reply, err)
+
+	var databases []string
+
+	for _, db := range dbs {
+		strs := strings.Split(db, ":")
+		dbi, _ := strconv.Atoi(strs[0][2:])
+		databases[dbi] = strs[1]
+	}
+	return databases, err
+}
+
+func (client RedisClient) Scan(cursor *uint64, match string, limit uint64) ([]string, error) {
+	reply, err := client.conn.Do("SCAN", cursor, match, limit)
+	result, err := redis.Values(reply, err)
+
+	var keys []string
+
+	for _, v := range result {
+		switch v.(type) {
+		case uint8:
+			*cursor, _ = redis.Uint64(v, nil)
+		case []string:
+			keys, _ = redis.Strings(v, nil)
+		}
+	}
+	return keys, err
+}
+
+func (client RedisClient) Ttl(key string) (uint64, error) {
+	reply, err := client.conn.Do("TTL", key)
+	ttl, err := redis.Uint64(reply, err)
+	return ttl, err
+}
+
+func (client RedisClient) SerializedLength(key string) (int, error) {
+	reply, err := client.conn.Do("DEBUG", "OBJECT", key)
+	debug, err := redis.String(reply, err)
+	debugs := strings.Split(debug, " ")
+	items := strings.Split(debugs[4], ":")
+	if err != nil {
+		return 0, err
+	}
+	return strconv.Atoi(items[1])
+}
